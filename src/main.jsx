076fc7e2bd9b9da8985dba40598558869a2e20a7
Jennifer <jenniferjude4@gmail.com>
2024-05-27 20:45:39 -0400

    I styled the layout container and removed App.css file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Missing from './pages/Missing.jsx'
@@ -12,7 +11,7 @@ import Contact from './components/Contact.jsx'
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <About />,
   },
   {
     path: "/about",
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from './Footer'
 
 function Layout({children}) {
   return (
-    <div>
+    <div className='layout-container'>
       <Header />
       <main>{children}</main>
       <Footer />
