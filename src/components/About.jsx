147c77a926c3bece41bf14d85111db9c5a8d37d8
Jennifer <jenniferjude4@gmail.com>
2024-05-27 20:16:21 -0400

    I created new files for the navigation

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../pages/Layout'
+
+function About() {
+  return (
+    <Layout>
+      <div>
+        <p>This is about section</p>
+      </div>
+    </Layout>
+  )
+}
+
+export default About
---
+++ src/pages/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <nav>
+        <a href='/about'>About</a>
+        <a href='/project'>Projects</a>
+        <a href='/services'>Services</a>
+        <a href='/contact'>Conatct</a>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
