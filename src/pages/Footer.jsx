2a0c12d07162a4247ee7564d84391cedf78c2b1c
Jennifer <jenniferjude4@gmail.com>
2024-05-27 22:27:23 -0400

    I worked on the footer component

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 
 function Footer() {
+  const year = new Date().getFullYear()
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <div>
+        <div>
+          <nav>
+            <a href='/about'>About</a>
+            <a href='/projects'>Projects</a>
+            <a href='/services'>Services</a>
+            <a href='/contact'>Contact</a>
+          </nav>
+        </div>
+        <div>
+          <h1>Subscribe to my Newsletter</h1>
+          <div>
+            <input type='text' />
+            <button>Subscribe</button>
+          </div>
+        </div>
+      </div>
+      <p>Copyright &copy; {year}. All rights reserved.</p>
+    </footer>
   )
 }
 
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
+    <header>
       <nav>
         <a href='/about'>About</a>
         <a href='/projects'>Projects</a>
         <a href='/services'>Services</a>
         <a href='/contact'>Contact</a>
       </nav>
-    </div>
+    </header>
   )
 }
 
