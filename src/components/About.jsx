79a24bb46aee341c61d51f104631f0b428990d03
Jennifer <jenniferjude4@gmail.com>
2024-05-27 21:18:25 -0400

    I worked on the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,28 @@ import React from 'react'
 import Layout from '../pages/Layout'
 
 function About() {
-  return (
-    <Layout>
-      <div>
-        <p>This is about section</p>
-      </div>
-    </Layout>
-  )
+    return (
+        <Layout>
+            <div>
+                <div>
+                    <img src='jennifer.jpg' alt='My profile image' />
+                </div>
+                <div>
+                    <p>Hello, my name is <br /> <span>Jennifer Arabel Jude</span></p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        Voluptates modi, ipsum harum eius eaque illo sit possimus
+                        illum quisquam fugiat, tenetur dolorum ducimus doloribus
+                        quo molestias? Architecto saepe ducimus aliquid dignissimos
+                        cum? Ut ea dicta, ab culpa totam facilis soluta excepturi
+                        consectetur fuga omnis et sunt harum vel tenetur quod.
+                        Eos error distinctio id nobis nulla hic accusamus
+                        corrupti soluta, velit, aliquam praesentium veniam.
+                    </p>
+                    <button>contact me</button>
+                </div>
+            </div>
+        </Layout>
+    )
 }
 
 export default About
